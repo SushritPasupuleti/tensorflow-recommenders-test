f211ffb24ec03686bbf70bf5be9a2813be3f153a
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-24 11:36:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#%%
 from typing import Dict, Text
 
 import numpy as np
@@ -5,3 +6,17 @@ import tensorflow as tf
 
 import tensorflow_datasets as tfds
 import tensorflow_recommenders as tfrs
+# %%
+# Ratings data.
+ratings = tfds.load('movie_lens/100k-ratings', split="train")
+# Features of all the available movies.
+movies = tfds.load('movie_lens/100k-movies', split="train")
+
+# Select the basic features.
+ratings = ratings.map(lambda x: {
+    "movie_title": x["movie_title"],
+    "user_id": x["user_id"]
+})
+movies = movies.map(lambda x: x["movie_title"])
+# %%
+print(movies)
